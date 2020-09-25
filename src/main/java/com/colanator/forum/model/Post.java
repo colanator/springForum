7ddbaa30dfba4b0846d67999c961921730ab0e57
Aapo Torkkeli <aamato@utu.fi>
2020-09-25 11:25:56 +0300

    Add getters and setters to Post
    This makes these show up in our json response

--- src/main/java/com/colanator/forum/model/Post.java
+++ src/main/java/com/colanator/forum/model/Post.java
@@ -9,9 +9,38 @@ import java.util.List;
 @Entity
 public class Post {
 
+	public Long getId() {
+		return id;
+	}
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
 	private String title;
 	private String body;
 	private String author;
