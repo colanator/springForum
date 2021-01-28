e0a1c998a38401d9ae63324146632fc1beb4d0aa
Aapo Torkkeli <aamato@utu.fi>
2021-01-28 19:50:41 +0200

    Cleanup ForumApplication
    Remove unnecessary imports.

--- src/main/java/com/colanator/forum/ForumApplication.java
+++ src/main/java/com/colanator/forum/ForumApplication.java
@@ -5,9 +5,6 @@ import com.colanator.forum.service.ContentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 
